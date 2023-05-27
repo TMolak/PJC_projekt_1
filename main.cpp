a1c54646c2c234b3159ff9586570ec24057d348e
TMolak <tomekmolak@gmail.com>
2023-05-27 16:06:01 +0200

    1.2.3 - tablica 0 i 1 z odwzorowaniem planszy

--- main.cpp
+++ main.cpp
@@ -3,57 +3,85 @@
 
 using namespace std;
 
+const int rows = 7;
+const int columns = 7;
+const float posX = 400.f;
+const float posY = 400.f;
+const float spacingX = 90.f;
+const float spacingY = 90.f;
+const float radius = 40.f;
+
+vector<sf::CircleShape> hexagons;
+
 
 int main() {
+    int boardPattern[17][33] = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,2,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,1,0,0,0,0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0},
+            {1, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+            {0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 1, 0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+
+    };
 //tworzenie okna
-    sf::RenderWindow window(sf::VideoMode({1000, 1000}), "Hexxagon",
+    sf::RenderWindow window(sf::VideoMode({1920, 1080}), "Hexxagon",
                             sf::Style::Default,
                             sf::ContextSettings(0, 0, 8));
 
     window.clear(sf::Color::White);
 
-        const int rows = 7;
-        const int columns = 7;
-        const float posX = 100.f;
-        const float posY = 100.f;
-        const float spacingX = 90.f;
-        const float spacingY = 90.f;
-        const float radius = 40.f;
-
-        vector<sf::CircleShape> hexagons;
-
-        window.clear(sf::Color::White);
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < columns - i; j++)
-            {
+    for (int i = 0; i < 17; i++) {
+        for (int j = 0; j < 33; j++) {
+            if(boardPattern[i][j] == 1){
                 sf::CircleShape hexagon(radius, 6);
                 hexagon.setFillColor(sf::Color::Magenta);
                 hexagon.setOutlineThickness(2.f);
                 hexagon.setOutlineColor(sf::Color::Black);
-                float x = posX + (spacingX * j) + (i * (spacingX / 2.f));
-                float y = posY + (spacingY * i);
+                float x = posX + (spacingX+radius);
+                float y = posY + (spacingY+radius);
 
                 hexagon.setPosition({x, y});
                 hexagons.push_back(hexagon);
-                window.draw(hexagon);
             }
         }
+    }
+
+//    for (int i = rows; i > 0; i--) {
+//        for (int j = 0; j < ; ++j) {
+//
+//        }
+//    }
+
+    for (sf::CircleShape hexagon: hexagons) {
+        window.draw(hexagon);
+    }
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed){
+            if (event.type == sf::Event::Closed) {
                 window.close();
-            }else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                for (sf::CircleShape& hexagon : hexagons) {
+            } else if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                for (sf::CircleShape &hexagon: hexagons) {
                     if (hexagon.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y))) {
                         hexagon.setFillColor(sf::Color::Green);
                     }
                 }
-           }
+            }
         }
-        for (const sf::CircleShape& hexagon : hexagons) {
+        for (const sf::CircleShape &hexagon: hexagons) {
             window.draw(hexagon);
         }
         window.display();
