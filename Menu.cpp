eec1ac6c93afd7e84ce0f618485b5401141df6ab
TMolak <tomekmolak@gmail.com>
2023-05-29 13:30:15 +0200

    1.3.6 - reorganizacja kodu, przeniesienie board jako klase

--- Menu.cpp
+++ Menu.cpp
@@ -1,13 +1,16 @@
 
 #include "Menu.h"
 
-Menu::Menu(float width, float height) {
-    text[0].setFont(font);
-    text[0].setOutlineColor(sf::Color::Red);
-    text[0].setString("Graj");
-    text[0].setPosition(sf::Vector2f(width / 2, height / 2));
-}
-
-Menu::~Menu() {
-
-}
+//Menu::Menu(float width, float height) {
+//    if (!font.loadFromFile("arial.ttf")) {
+//        // TODO
+//    }
+//    text[0].setFont(font);
+//    text[0].setOutlineColor(sf::Color::Red);
+//    text[0].setString("Graj");
+//    text[0].setPosition(sf::Vector2f(width / 2, height / 2));
+//}
+//
+//Menu::~Menu() {
+//
+//}
--- main.cpp
+++ main.cpp
@@ -3,13 +3,65 @@
 #include "Menu.h"
 using namespace std;
 
-const float posX = 100.f;
-const float posY = 100.f;
-const float spacingX = 60.f;
-const float spacingY = 60.f;
 const float radius = 30.0f;
 
-vector<sf::CircleShape> hexagons;
+
+
+class Board{
+public:
+    Board(int pattern[17][9]){
+        for (int i = 0; i < 17; i++) {
+            for (int j = 0; j < 9; j++) {
+                sf::CircleShape hexagon(radius, 6);
+                sf::Angle rotation(90);
+                hexagon.setOutlineColor(sf::Color::Transparent);
+                if (pattern[i][j] == 1) {
+                    hexagon.setFillColor(sf::Color::Magenta);
+                    hexagon.setOutlineColor(sf::Color::Black);
+                } else if (pattern[i][j] == 2) {
+                    hexagon.setFillColor(sf::Color::Transparent);
+                }else if (pattern[i][j] == 3) {
+                    hexagon.setFillColor(sf::Color::Yellow);
+                    hexagon.setOutlineColor(sf::Color::Black);
+                }else if (pattern[i][j] == 4) {
+                    hexagon.setFillColor(sf::Color::Red);
+                    hexagon.setOutlineColor(sf::Color::Black);
+                }else{
+                    hexagon.setFillColor(sf::Color::Transparent);
+                }
+                hexagon.setOutlineThickness(2.f);
+
+                float x = radius * j * 3.4;
+                float y = radius * i;
+                if (i % 2 != 0 && i != 0) {
+                    x += radius * 1.7;
+                }
+                hexagon.setPosition({x, y});
+                hexagon.rotate(rotation);
+                hexagons.push_back(hexagon);
+
+            }
+        }
+    }
+    void draw(sf::RenderWindow& window){
+        for (const sf::CircleShape &hexagon: hexagons) {
+            window.draw(hexagon);
+        }
+    }
+
+    void mouseClick(const sf::Event& event){
+        if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+            for (sf::CircleShape &hexagon: hexagons) {
+                if (hexagon.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) &&
+                    hexagon.getFillColor() != sf::Color::Transparent && hexagon.getFillColor() != sf::Color::Blue) {
+                    hexagon.setOutlineColor(sf::Color::Cyan);
+                }
+            }
+        }
+    }
+private:
+    vector<sf::CircleShape> hexagons;
+};
 
 
 int main() {
@@ -42,39 +94,9 @@ int main() {
 
     window.clear(sf::Color::White);
 
+Board board(boardPattern);
 
-    for (int i = 0; i < 17; i++) {
-        for (int j = 0; j < 9; j++) {
-            sf::CircleShape hexagon(radius, 6);
-            sf::Angle rotation(90);
-            hexagon.setOutlineColor(sf::Color::White);
-            if (boardPattern[i][j] == 1) {
-                hexagon.setFillColor(sf::Color::Magenta);
-                hexagon.setOutlineColor(sf::Color::Black);
-            } else if (boardPattern[i][j] == 2) {
-                hexagon.setFillColor(sf::Color::Blue);
-                hexagon.setOutlineColor(sf::Color::Black);
-            }else if (boardPattern[i][j] == 3) {
-                hexagon.setFillColor(sf::Color::Yellow);
-                hexagon.setOutlineColor(sf::Color::Black);
-            }else if (boardPattern[i][j] == 4) {
-                hexagon.setFillColor(sf::Color::Red);
-                hexagon.setOutlineColor(sf::Color::Black);
-            }
-            hexagon.setOutlineThickness(2.f);
 
-            float x = radius * j * 3.4;
-            float y = radius * i;
-            if (i % 2 != 0 && i != 0) {
-                x += radius * 1.7;
-            }
-            hexagon.setPosition({x, y});
-            hexagon.rotate(rotation);
-            hexagons.push_back(hexagon);
-            window.draw(hexagon);
-
-        }
-    }
 
 
     while (window.isOpen()) {
@@ -83,18 +105,11 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                for (sf::CircleShape &hexagon: hexagons) {
-                    if (hexagon.getGlobalBounds().contains(sf::Vector2f(event.mouseButton.x, event.mouseButton.y)) &&
-                        hexagon.getFillColor() != sf::Color::White && hexagon.getFillColor() != sf::Color::Blue) {
-                        hexagon.setOutlineColor(sf::Color::Cyan);
-                    }
-                }
+            if (event.type == sf::Event::MouseButtonPressed){
+                board.mouseClick(event);
             }
         }
-        for (const sf::CircleShape &hexagon: hexagons) {
-            window.draw(hexagon);
-        }
+        board.draw(window);
         window.display();
     }
 }
